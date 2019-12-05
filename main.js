c7a00827d736db6d399ab4f6aa07ad0aa21099f9
CodeDrip <dhaesha@gmail.com>
2019-12-05 09:47:40 -0500

    created seperate quiz script

--- main.js
+++ main.js
@@ -3,19 +3,25 @@ console.log("Push Thruuuu")
 var startGame = function() {
     var a;
 }
+var currentQuestion = 0;
+var totalQuestions = quiz.length;
+var score = 0;
 var points;
 // var currentQuestion;
 // var totalQuestions = arr.length;
+
+
 var plyrChoice;
 var correctChoice;
 
-showQuestion = document.getElementsByTagName(h2).innerhtml
-
-firstCh = document.getElementsByClassName(first-ch).innerhtml
-secondCh = document.getElementsByClassName(second-ch).innerhtml
-thirdCh = document.getElementsByClassName(third-ch).innerhtml
-fourthCh = document.getElementsByClassName(fourth-ch).innerhtml
-
+var container = document.getElementsByTagName("section")
+var showQuestion = document.getElementsByTagName("h2").innerhtml
+var firstCh = document.getElementsByClassName("first-ch").innerhtml
+var secondCh = document.getElementsByClassName("second-ch").innerhtml
+var thirdCh = document.getElementsByClassName("third-ch").innerhtml
+var fourthCh = document.getElementsByClassName("fourth-ch").innerhtml
+var answer;
+var nextButton = document.getElementById("nexBtn")
 
 // var game {}
 
---
+++ questions.js
@@ -0,0 +1,132 @@
+let quiz = [
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    {
+        question: "Quiz Question",
+        firstCh: "Answer",
+        secondCh: "answer",
+        thirdCh: "Another",
+        fourthCh: "Last",
+        answer: firstCh
+    },
+    
+
+]
