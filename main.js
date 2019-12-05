fa46d9798d31dc9c95164ae1363b4a453649b80e
CodeDrip <dhaesha@gmail.com>
2019-12-04 20:34:30 -0500

    changed p's to li's

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("Push Thruuuu")
+
+var startGame = function() {
+    var a;
+}
+var points;
+// var currentQuestion;
+// var totalQuestions = arr.length;
+var plyrChoice;
+var correctChoice;
+
+showQuestion = document.getElementsByTagName(h2).innerhtml
+
+firstCh = document.getElementsByClassName(first-ch).innerhtml
+secondCh = document.getElementsByClassName(second-ch).innerhtml
+thirdCh = document.getElementsByClassName(third-ch).innerhtml
+fourthCh = document.getElementsByClassName(fourth-ch).innerhtml
+
+
+// var game {}
+
